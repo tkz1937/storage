f775f75ce372463bda5c834da6f5eff79f14510b
maker1937 <tekazayaeliel@gmail.com>
2018-08-29 14:05:01 +0200

    [UPDATE]

---
+++ views/devices/add.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Partition summary view.
+ *
+ * @category   apps
+ * @package    storage
+ * @subpackage views
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2013 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/storage/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+//  
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// Load dependencies
+///////////////////////////////////////////////////////////////////////////////
+
+$this->lang->load('base');
+$this->lang->load('storage');
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Form
+///////////////////////////////////////////////////////////////////////////////
+
+
+echo form_open('storage/devices/add');
+
+echo form_header(lang('Add_device'));
+
+echo field_input('device', '', '' , FALSE);
+echo form_submit_custom('save','Save');
+
+echo form_footer();
+if ($error == 1) {
+	echo infobox_warning(lang('create_error'));
+}
+echo form_close();
